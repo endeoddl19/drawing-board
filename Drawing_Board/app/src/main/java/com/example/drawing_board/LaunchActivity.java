ac39d19b943ed43bb165304801f7453616e6ce8c
endeo <endeoddl19>
2021-01-20 17:12:19 +0900

    splash screen 추가

---
+++ Drawing_Board/app/src/main/java/com/example/drawing_board/LaunchActivity.java
@@ -0,0 +1,22 @@
+package com.example.drawing_board;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    private static final int splash = 1500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        new Handler().postDelayed(()->{
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            },splash);
+    }
+}
