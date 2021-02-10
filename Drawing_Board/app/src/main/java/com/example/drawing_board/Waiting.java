ee048ef5531d55600a9612b7eb2160681b2e6d13
endeo <endeoddl19>
2021-02-10 18:57:18 +0900

    waiting 구상중

--- Drawing_Board/app/src/main/java/com/example/drawing_board/Waiting.java
+++ Drawing_Board/app/src/main/java/com/example/drawing_board/Waiting.java
@@ -10,22 +10,25 @@ import android.widget.TextView;
 public class Waiting extends AppCompatActivity {
 
     private int curplayers,maxplayers;
-    private TextView roomid, players;
-    private String room_id;
+    private TextView roomid, players_tv;
+    private String room_id,players_st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting);
 
-        Intent intent = new Intent();
+        Intent intent = new Intent(); // Create / Menu 에서 받아올 정보들
         intent.getIntExtra("playernum",1);
         intent.getIntExtra("maxplayers",4);
         room_id = intent.getStringExtra("roomid");
 
         roomid = findViewById(R.id.room_id_waiting);
-        players = findViewById(R.id.players_waiting);
+        players_tv = findViewById(R.id.players_waiting);
+
+        players_st = (curplayers+" / "+maxplayers);
 
         roomid.setText(room_id);
+        players_tv.setText(players_st);
     }
 }
